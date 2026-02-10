31e8185df1d00f1be5255f974ef2e22460037952
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-10 12:43:54 -0600

    Repository

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T: class
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        IQueryable<T> GetQueryable();
+    }
+}
