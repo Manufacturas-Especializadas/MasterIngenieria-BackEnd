abcb8ef35e5f6dd5fb4dffbcde6614772f9cbdee
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-11 09:54:58 -0600

    Dto de kpis

---
+++ Application/Dtos/KpiStatsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class KpiStatsDto
+    {
+        public int TotalUniqueParts { get; set; }
+
+        public int MaxProcessLoad { get; set; }
+
+        public string MaxProcessName { get; set; } = string.Empty;
+
+        public int TotalProcessesCount { get; set; }
+
+        public List<ProcessStatsDto> StatsByProcess { get; set; } = new();
+    }
+}
