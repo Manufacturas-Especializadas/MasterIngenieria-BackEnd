1264eb8c83809091ca3b0b6355b541778a19b4ff
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-25 10:19:38 -0600

    feat: dto de tiempos de ciclo

---
+++ Application/Dtos/TopCycleTimeDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class TopCycleTimeDto
+    {
+        public string PartNumber { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal TCiclo { get; set; }
+
+        public string Operation {  get; set; }
+    }
+}
