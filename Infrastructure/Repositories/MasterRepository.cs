d12a72e526b7d31a9c25e41109f4ffac9738c70d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-25 10:17:56 -0600

    feat: master repository con el metodo para obtener los tiempos de ciclo mas altos

---
+++ Infrastructure/Repositories/MasterRepository.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class MasterRepository : IMasterRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MasterRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Master>> GetTopCycleTimesByLineAsync(int line, int top = 5)
+        {
+            return await _context.Masters
+                .Where(m => m.Line == line && m.TCiclo.HasValue)
+                .OrderByDescending(m => m.TCiclo)
+                .ThenBy(m => m.ParentPartNumber)
+                .Take(top)
+                .ToListAsync();
+        }
+    }
+}
