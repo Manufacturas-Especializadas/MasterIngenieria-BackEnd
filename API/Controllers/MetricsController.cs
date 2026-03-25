078296b91168acbf3c7bef3af54d1f1a4ef44f9b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-25 10:20:16 -0600

    feat: controlador con el endpoint para obtener los tiempos de ciclo mas alto

---
+++ API/Controllers/MetricsController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Dtos;
+using Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MetricsController : ControllerBase
+    {
+        private readonly MasterService _masterService;
+
+        public MetricsController(MasterService masterService) => _masterService = masterService;
+
+        [HttpGet]
+        [Route("top-cycle-times/{line}")]
+        public async Task<ActionResult<TopCycleTimeDto>> GetTopByLine(int line)
+        {
+            var result = await _masterService.GetTopFiveByLine(line);
+
+            if (!result.Any()) return NotFound("No se encontrarón registros para esta línea");
+
+            return Ok(result);
+        }
+    }
+}
