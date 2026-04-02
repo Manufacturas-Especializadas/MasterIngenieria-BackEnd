1c19669436126d0c2be37796b1c0ca5446d589d9
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-02 08:26:02 -0600

    feat: mapeo de propiedades

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -27,11 +27,22 @@ namespace Infrastructure.Data
                 entity.Property(e => e.ChildPartNumber).HasColumnName("childPartNumber");
                 entity.Property(e => e.ExternalDiameter).HasColumnName("externalDiameter");
                 entity.Property(e => e.WallThickness).HasColumnName("wallThickness");
+                entity.Property(e => e.Development).HasColumnName("development");
                 entity.Property(e => e.Description).HasColumnName("description");
                 entity.Property(e => e.Type).HasColumnName("type");
+                entity.Property(e => e.Family).HasColumnName("family");
+                entity.Property(e => e.Client).HasColumnName("client");
+                entity.Property(e => e.Line).HasColumnName("line");
+                entity.Property(e => e.PartOfPurchase).HasColumnName("partOfPurchase");
+                entity.Property(e => e.QuantityXQuantity).HasColumnName("quantityXQuantity");
                 entity.Property(e => e.Operation).HasColumnName("operation");
                 entity.Property(e => e.Sequence).HasColumnName("sequence");
+                entity.Property(e => e.ProcessComments).HasColumnName("processComments");
+                entity.Property(e => e.MajorSetup).HasColumnName("majorSetup");
+                entity.Property(e => e.MinorSetup).HasColumnName("minorSetup");
                 entity.Property(e => e.PzsHr).HasColumnName("pzsHr");
+                entity.Property(e => e.Verification).HasColumnName("verification");
+
                 entity.Property(e => e.Oper)
                     .HasColumnType("decimal(10, 3)")
                     .HasColumnName("oper");
