16a069b2c1a60d24c1333d91103dca5f4a898687
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-02 08:30:43 -0600

    feat: Modelo de sincronizacion

---
+++ Core/Models/SyncResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class SyncResult
+    {
+        public bool Success { get; set; }
+
+        public int TotalRecords { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public double ExecutionTimeSeconds { get; set; }
+
+        public DateTime SyncDate { get; set; } = DateTime.Now;
+    }
+}
