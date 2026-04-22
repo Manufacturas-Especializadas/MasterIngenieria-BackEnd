94994fff9709fa9ecb876766b7f840b2356d09e7
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-22 07:31:11 -0600

    feat: dto de la entidad MasterImprovement

---
+++ Application/Dtos/MasterImprovementDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class MasterImprovementDto
+    {
+        public string ParentPartNumber { get; set; } = null!;
+        public decimal OldCycleTime { get; set; }
+
+        public decimal NewCycleTime { get; set; }
+
+        public decimal TimeSaved => OldCycleTime - NewCycleTime;
+
+        public decimal PercentImprovement => OldCycleTime > 0
+            ? Math.Round(((OldCycleTime - NewCycleTime) / OldCycleTime) * 100, 2)
+            : 0;
+
+        public string? Process { get; set; }
+
+        public DateTime ImprovementDate { get; set; }
+    }
+}
